e98d51802f6a26174fb08cc5b77a63a88d670b0c
Ivan Grabovsky <raccoon.twitch@gmail.com>
2021-02-25 12:00:41 +0500

    added hello request

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import requests
-import aiohttp
+from aiohttp import web
 
+routes = web.RouteTableDef()
 
+@routes.get('/')
+async def hello(request):
+    name = request.query['name']
+    txt = "Hello, {}".format(name)
+    return web.Response(text=txt)
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app)
