2cd6c39f3a17bf92cacc2a9db4271517d9d4062d
Ivan Grabovsky <raccoon.twitch@gmail.com>
2021-02-25 16:42:18 +0500

    added procents

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 from aiohttp import web
 import pymongo
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import json
 #import dnspython
+
 
 routes = web.RouteTableDef()
 client = pymongo.MongoClient("mongodb+srv://admin:RTF4empion@cluster0.p8umr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.IOT
 mydb = client["IOT"]
 mycol = mydb["users"]
 
@@ -13,17 +16,25 @@ mycol = mydb["users"]
 #myCursor = mycol.find({"name":"Ivan"})
 #x = mycol.insert_one(mydict)
 
-@routes.get('/auth')
-async def hello(request):
-    username = request.query['username']
-    password = request.query['password']
-    usernameDB = list(mycol.find({"username": username}))
-    if len(usernameDB) == 0:
-        return web.Response(text="Not found")
-    first = usernameDB[0]
-    if password == first["password"]:
-        return web.Response(text="True")
-    return web.Response(text="False")
+@routes.get('/percents')
+async def procents(request):
+    request_data = dict(request.query)
+    user_id = request_data['id']
+    user_info = mycol.find_one({"_id": ObjectId(user_id)})
+    user_hard = user_info['hard']
+    user_soft = user_info['soft']
+    courses = list(mydb["courses"].find({}))
+    procents_dict = {}
+    for elem in courses:
+        course_hard = elem['hard']
+        dif = sum(1 for v in user_hard if v in course_hard)
+        procents_dict[elem['name']] = round(dif / len(course_hard) * 100)
+    result = []
+    for elem in courses:
+        dictionary = {'_id': elem['_id'], 'name': elem['name'], 'procent': procents_dict[elem['name']]}
+        result.append(dictionary)
+    return web.Response(text=str(result))
+
 
 
 @routes.get('/')
