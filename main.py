16fdab055c7ad8bbc9df7986ab96d4be07a58133
Ivan Grabovsky <raccoon.twitch@gmail.com>
2021-02-25 17:14:17 +0500

    fix PR

--- main.py
+++ main.py
@@ -4,6 +4,19 @@ import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import json
+import asyncio
+
+import motor.motor_asyncio as aiomotor
+from aiohttp_session_mongo import MongoStorage
+from aiohttp import web
+from aiohttp_session import setup, get_session, session_middleware, SimpleCookieStorage
+from aiohttp_security import setup as setup_security, SessionIdentityPolicy
+
+import logging
+
+from auth.db import MongoUserRepository
+from auth.db_auth import DBAuthorizationPolicy
+from auth.views import Auth
 #import dnspython
 
 
@@ -35,15 +48,41 @@ async def procents(request):
         result.append(dictionary)
     return web.Response(text=str(result))
 
+async def init_mongo(loop):
+    url = "mongodb+srv://admin:RTF4empion@cluster0.p8umr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    conn = aiomotor.AsyncIOMotorClient(
+        url, maxPoolSize=2, io_loop=loop)
+    db = 'IOT'
+    return conn[db]
 
+async def setup_mongo(loop):
+    db = await init_mongo(loop)
+    return db
 
 @routes.get('/')
 async def hello(request):
     return web.Response(text="true")
 
-app = web.Application()
-app.add_routes(routes)
+async def make_app():
+    max_age = 3600 * 24 * 365
+    loop = asyncio.get_event_loop()
+    db = await setup_mongo(loop)
+    session_collection = db['sessions']
+    middleware = session_middleware(MongoStorage(session_collection, max_age=max_age))
 
-web.run_app(app)
+    app = web.Application(middlewares=[middleware])
+
+    async def close_mongo():
+        db.client.close()
+    app.on_cleanup.append(close_mongo)
+
+    app.user_repo = MongoUserRepository(db)
+    policy = SessionIdentityPolicy()
+    setup_security(app, policy, DBAuthorizationPolicy(app.user_repo))
+    auth_handlers = Auth()
+    auth_handlers.configure(app)
+    app.add_routes(routes)
+    return app
+web.run_app(make_app())
 
 
