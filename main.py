c920956e1662a0df2e6c7088f5150c2663736997
Ivan Grabovsky <raccoon.twitch@gmail.com>
2021-02-25 12:09:59 +0500

    added work with database

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import requests
 from aiohttp import web
+import pymongo
+#import dnspython
 
 routes = web.RouteTableDef()
+client = pymongo.MongoClient("mongodb+srv://admin:RTF4empion@cluster0.p8umr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+mydb = client["mydatabase"]
+mycol = mydb["customers"]
+
+mydict = { "name": "John", "address": "Highway 37" }
+
+#x = mycol.insert_one(mydict)
 
 @routes.get('/')
 async def hello(request):
@@ -11,4 +21,5 @@ async def hello(request):
 
 app = web.Application()
 app.add_routes(routes)
+
 web.run_app(app)
