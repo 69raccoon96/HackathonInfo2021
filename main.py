f348876e80d4e98ad08fc2345aa6429511fd2326
Ivan Grabovsky <raccoon.twitch@gmail.com>
2021-02-25 13:17:14 +0500

    auth completed

--- main.py
+++ main.py
@@ -5,21 +5,34 @@ import pymongo
 
 routes = web.RouteTableDef()
 client = pymongo.MongoClient("mongodb+srv://admin:RTF4empion@cluster0.p8umr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
-mydb = client["mydatabase"]
-mycol = mydb["customers"]
+db = client.IOT
+mydb = client["IOT"]
+mycol = mydb["users"]
 
-mydict = { "name": "John", "address": "Highway 37" }
-
+#mydict = { "name": "John", "address": "Highway 37" }
+#myCursor = mycol.find({"name":"Ivan"})
 #x = mycol.insert_one(mydict)
+
+@routes.get('/auth')
+async def hello(request):
+    username = request.query['username']
+    password = request.query['password']
+    usernameDB = list(mycol.find({"username": username}))
+    if len(usernameDB) == 0:
+        return web.Response(text="Not found")
+    first = usernameDB[0]
+    if password == first["password"]:
+        return web.Response(text="True")
+    return web.Response(text="False")
+
 
 @routes.get('/')
 async def hello(request):
-    name = request.query['name']
-    txt = "Hello, {}".format(name)
-    return web.Response(text=txt)
+    return web.Response(text="true")
 
 app = web.Application()
 app.add_routes(routes)
 
 web.run_app(app)
+
+
